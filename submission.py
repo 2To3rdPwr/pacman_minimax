797fde4c788f45ad3c0c17db187c0c50e001e246
JSage <jsage@callbox.com>
2020-07-20 16:31:15 -0400

    Minimax implementation

--- submission.py
+++ submission.py
@@ -1,3 +1,5 @@
+import sys
+
 from util import manhattanDistance
 from game import Directions
 import random, util
@@ -161,10 +163,47 @@ class MinimaxAgent(MultiAgentSearchAgent):
         The depth to which search should continue
 
     """
+    return self.minimax(gameState, self.depth, 0)
 
-    # BEGIN_YOUR_CODE (our solution is 26 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
-    # END_YOUR_CODE
+  def minimax(self, state, depth, player):
+    """
+    @author: Jake Sage
+    :param state: gameState at the given node of the minimax tree
+    :param depth: How deep in the minimax tree we are currently
+    :param player: 0 = Pac-Man, 1+ = Ghosts
+    :return: While recurring, the score chosen by minimax at this node
+              At the tree root, return the action Pac-Man will take
+    """
+    if depth == 0 or state.isWin() or state.isLose():
+      # End States
+      return state.getScore()
+    elif player == 0:
+      # Pac-Man (Max Agent)
+      maxAction = -1 * sys.maxint
+      actions = state.getLegalActions(player)
+      for action in actions:
+        result = self.minimax(state.generateSuccessor(player, action), depth, player + 1)
+        if maxAction == result:     # randomize result of ties to help break up thrashing
+          bestAction = random.choice([bestAction, action])
+        elif maxAction < result:
+          maxAction = result
+          bestAction = action
+      if depth == self.depth:
+        return bestAction
+      else:
+        return maxAction
+    else:
+      # Ghosts (Min Agent)
+      minAction = sys.maxint
+      actions = state.getLegalActions(player)
+      for action in actions:
+        if player + 1 < state.getNumAgents():
+          result = self.minimax(state.generateSuccessor(player, action), depth, player + 1)
+        else:
+          result = self.minimax(state.generateSuccessor(player, action), depth - 1, 0)
+        if minAction > result:
+          minAction = result
+      return minAction
 
 ######################################################################################
 # Problem 2a: implementing alpha-beta
